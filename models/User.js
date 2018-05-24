0a22b46ba3b7663c9043d5718ba440bc158c0ed3
buyongfeng <buyongfeng@gmail.com>
2018-05-24 15:32:03 +0800

    :tada: register

--- models/User.js
+++ models/User.js
@@ -1,2 +1,50 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/test', { userMongoClinent: true })
+mongoose.connect('mongodb://localhost/test')
+
+var schema = mongoose.Schema
+
+var userSchema = new schema({
+    email: {
+        type: String,
+        required: true
+    },
+    nickname: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        default: '/public/img/avatar-default.png'
+    },
+    bio: {
+        type: String,
+        default: ''
+    },
+    gender: {
+        type: Number,
+        enum: [-1, 0, 1],
+        default: -1
+    },
+    birthday: {
+        type: Date
+    },
+    status: {
+        type: Number,
+        enum: [0, 1, 2],
+        default: 0
+    },
+    add_time: {
+        type: Date,
+        default: Date.now
+    },
+    modify_time: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
--- router.js
+++ router.js
@@ -1,4 +1,6 @@
 var express = require('express')
+var md5 = require('blueimp-md5')
+var User = require('./models/user')
 var router = express.Router()
 
 router.get('/', function(req, res) {
@@ -12,9 +14,29 @@ router.get('/login', function(req, res) {
 router.post('/login', function(req, res) {
     var body = req.body
     console.log(body)
-    res.status(200).json({
-        err_code: 0,
-        message: 'ok'
+    //查询user
+    User.findOne({
+        email: body.email,
+        password: md5(md5(body.password))
+    }, function(err, user) {
+        console.log(err);
+        console.log(user);
+        if (err) {
+            return next(err)
+        }
+
+        if (!user) {
+            return res.status(200).json({
+                err_code: 1,
+                message: 'invalid Email or Password'
+            })
+        }
+
+        res.status(200).json({
+            err_code: 0,
+            message: 'ok'
+        })
+
     })
 })
 
@@ -24,8 +46,41 @@ router.get('/logout', function(req, res) {
 })
 
 router.get('/register', function(req, res) {
-    console.log('注册')
-    res.end('注册')
+    res.render('register.html')
+})
+
+router.post('/register', function(req, res) {
+    var body = req.body
+    User.findOne({
+        $or: [{
+            email: body.email
+        }, {
+            nickname: body.nickname
+        }]
+    }, function(err, data) {
+        if (err) {
+            return next(err)
+        }
+        //是否存在
+        if (data) {
+            return res.status(200).json({
+                err_code: 1,
+                message: 'email or nickname is exists'
+            })
+        }
+        //注册
+        body.password = md5(md5(body.password))
+        new User(body).save(function(err,user){
+            if(err){
+                return next(err)
+            }
+            res.status(200).json({
+                err_code: 0,
+                message: 'ok'
+            })
+        })        
+
+    })
 })
 
 
